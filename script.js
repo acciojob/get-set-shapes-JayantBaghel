6fae78968196f21334e8ba7bf57b99b1a5459032
JayantBaghel <94191857+JayantBaghel@users.noreply.github.com>
2024-06-28 08:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 //complete this code
 class Rectangle {
 	    constructor(width, height) {
-        this._width = width;
-        this._height = height;
+        this.width = width;
+        this.height = height;
     }
 
     get width() {
-        return this._width;
+        return this.width;
     }
 
     get height() {
-        return this._height;
+        return this.height;
     }
 
     getArea() {
-        return this._width * this._height;
+        return this.width * this.height;
     }
 }
 
-class Square extends Animal {
+class Square extends Rectangle {
 	 constructor(side) {
         super(side, side);
     }
